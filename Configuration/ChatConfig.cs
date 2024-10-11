00db93bd36f9b3e8a6fa0f70b5139a0d6c708a8c
SLA_TheFrieber <wasnzeit@gmail.com>
2024-10-11 23:16:45 +0200

    History S/V base

---
+++ Configuration/ChatConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koboldcs.Configuration
+{
+    public static class ChatConfig
+    {
+    }
+}
